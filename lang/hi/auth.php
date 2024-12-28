116ee268bfccf7843e829d2a7de6b4e719dc4b2c
Deepak Kumar Thakur <85386465+deepakmhthakur@users.noreply.github.com>
2024-12-28 11:28:57 +0530

    Initial Commit

---
+++ lang/hi/auth.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'password' => 'पासवर्ड',
+    'throttle' => 'बहुत अधिक लॉगिन प्रयास। कृपया :seconds सेकंड में पुनः प्रयास करें।',
+    'save' => 'सहेजें',
+    'failed' => 'ये प्रमाण हमारे रिकॉर्ड से मेल नहीं खाते।',
+    'full_name' => 'पूरा नाम',
+    'email' => 'ईमेल',
+    'confirm_password' => 'पासवर्ड की पुष्टि करें',
+    'remember_me' => 'मुझे याद रखें',
+    'sign_in' => 'साइन इन करें',
+    'sign_out' => 'साइन आउट',
+    'register' => 'रजिस्टर करें',
+    'login' => [
+        'login' => 'लॉगिन',
+        'title' => 'अपना सत्र शुरू करने के लिए साइन इन करें',
+        'forgot_password' => 'पासवर्ड भूल गए',
+        'register_membership' => 'नई सदस्यता के लिए पंजीकरण करें',
+    ],
+    'registration' => [
+        'patient_registration' => 'रोगी पंजीकरण',
+        'already_user' => 'पहले से खाता है',
+        'i_agree' => 'मैं सहमत हूं',
+        'terms' => 'नियम और शर्तों से',
+        'have_membership' => 'मेरे पास पहले से ही सदस्यता है',
+    ],
+    'forgot_password' => [
+        'title' => 'अपना पासवर्ड रीसेट करने के लिए ईमेल दर्ज करें',
+        'send_pwd_reset' => 'पासवर्ड रीसेट लिंक भेजें',
+    ],
+    'reset_password' => [
+        'title' => 'अपना पासवर्ड रीसेट करें',
+        'reset_pwd_btn' => 'पासवर्ड रीसेट करें',
+        'already_reset' => 'पहले ही पासवर्ड रीसेट कर चुके हैं?',
+    ],
+    'new_here' => 'यहाँ नए हैं?',
+    'create_an_account' => 'खाता बनाएं',
+    'please_wait' => 'कृपया प्रतीक्षा करें...',
+    'submit' => 'जमा करें',
+    'already_user' => 'पहले से उपयोगकर्ता?',
+    'logins' => 'लॉगिन',
+
+];
---
+++ lang/hi/web.php
@@ -0,0 +1,110 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | All Titles and static string in blade files
+    |
+    | Web Front side localization - English Language
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    //Header Keys
+    'departments' => 'विभाग',
+    'backend_features' => 'बैकेंड फीचर्स',
+    'testimonials' => 'प्रशंसापत्र',
+    'login' => 'लॉगिन',
+    'register' => 'रजिस्टर करें',
+    'start_using_InfyHMS_now' => 'अब InfyHMS का उपयोग शुरू करें।',
+    'get_started' => 'शुरू करें',
+    'welcome_to' => 'स्वागत है',
+    'infyhms' => 'InfyHMS',
+    'manage_your_hospital_day_to_day_operations_digitally_with_ease_and_effortlessly' => 'अपने अस्पताल के दिन-प्रतिदिन के संचालन को डिजिटल रूप से आसानी से और बिना किसी परेशानी के प्रबंधित करें।',
+    'buy_now' => 'अब खरीदें',
+    'home' => 'होम',
+    'about_us' => 'हमारे बारे में',
+
+    //Features keys
+    'features' => [
+        'features' => 'विशेषताएँ',
+        'emergency_services' => 'आपातकालीन सेवाएँ',
+        'qualified_doctors' => 'कुशल डॉक्टर',
+        'outdoors_checkup' => 'आउटडोर चेकअप',
+        'hours_services' => '24 घंटे सेवा',
+        'we_are_providing_advanced_emergency_services_We_have_well-equipped_emergency_and_trauma_center_with_facilities' => 'हम उन्नत आपातकालीन सेवाएँ प्रदान कर रहे हैं। हमारे पास सुविधाओं के साथ एक अच्छी तरह से सुसज्जित आपातकालीन और ट्रॉमा केंद्र है।',
+        'our_team_of_pathologists_microbiologists_and_clinical_laboratory_scientists_are_always_ready_to_help_you_with_your_laboratory_needs' => 'हमारी पैथोलॉजिस्ट, सूक्ष्मजीवविज्ञानी और क्लिनिकल प्रयोगशाला वैज्ञानिकों की टीम हमेशा आपकी प्रयोगशाला आवश्यकताओं में मदद करने के लिए तैयार रहती है।',
+        'our_doctors_are_always_ready_for_outdoor_checkup_in_an_emergency_we_have_different_types_of_charges_as_per_checkup' => 'हमारे डॉक्टर हमेशा आपातकाल में आउटडोर चेकअप के लिए तैयार रहते हैं। हमारे पास चेकअप के अनुसार विभिन्न प्रकार के शुल्क हैं।',
+        'our_clinic_provides_extensive_medical_support_and_healthcare_services_24_7' => 'हमारी क्लिनिक 24/7 व्यापक चिकित्सा समर्थन और स्वास्थ्य देखभाल सेवाएं प्रदान करती है।',
+    ],
+
+    //Backend Features keys
+    'backend_feature' => [
+        'dashboard' => 'डैशबोर्ड',
+        'change_password' => 'पासवर्ड बदलें',
+        'change_language' => 'भाषा बदलें',
+        'invoice_listing' => 'चालान सूची',
+        'create_invoice' => 'चालान बनाएं',
+        'create_bill' => 'बिल बनाएं',
+        'appointments' => 'नियुक्तियाँ',
+        'bed_listing' => 'बिस्तर सूची',
+        'bed_details' => 'बिस्तर विवरण',
+        'bed_allotment' => 'बिस्तर आवंटन',
+        'document' => 'दस्तावेज',
+        'add_ambulance' => 'एंबुलेंस जोड़ें',
+        'create_insurance' => 'बीमा बनाएं',
+        'create_doctor' => 'डॉक्टर बनाएं',
+        'create_medicine' => 'दवा बनाएं',
+        'add_employee_payroll_details' => 'कर्मचारी वेतन विवरण जोड़ें',
+        'employee_payroll_details' => 'कर्मचारी वेतन विवरण',
+        'payment_reports' => 'भुगतान रिपोर्ट',
+        'enquiry_listing' => 'प्रश्न सूची',
+        'patient_admission_listing' => 'रोगी प्रवेश सूची',
+        'doctor_schedules' => 'डॉक्टर का कार्यक्रम',
+        'birth_report_listing' => 'जन्म रिपोर्ट सूची',
+        'email_service' => 'ईमेल सेवा',
+        'settings' => 'सेटिंग्स',
+    ],
+
+    //MISCELLANEOUS FACILITIES keys
+    'miscellaneous_facilities' => [
+        'miscellaneous_facilities_of_infyhms' => 'INFYHMS की विविध सुविधाएँ',
+        'host_in_your_Own_secure_server' => 'अपने सुरक्षित सर्वर में होस्ट करें।',
+        'no_monthly_or_yearly_fees' => 'कोई मासिक या वार्षिक शुल्क नहीं।',
+        'customer_support' => '24/7 ग्राहक सहायता',
+        'multi_language_system' => 'बहु-भाषा प्रणाली',
+        'admin_and_customer_has_separate_ui_and_login' => 'एडमिन और ग्राहक का अलग UI और लॉगिन है',
+        'email_and_sms_gateway_adding_for_marketing' => 'मार्केटिंग के लिए ईमेल और एसएमएस गेटवे जोड़ना',
+        'complete_hospital_management_solution' => 'पूर्ण अस्पताल प्रबंधन समाधान',
+        'prescription_management_system' => 'प्रिस्क्रिप्शन प्रबंधन प्रणाली',
+        'doctor_management_system' => 'डॉक्टर प्रबंधन प्रणाली',
+        'insurance_management_system' => 'बीमा प्रबंधन प्रणाली',
+        'billing_management_system' => 'बिलिंग प्रबंधन प्रणाली',
+        'role_based_permission_setup_system' => 'भूमिका-आधारित अनुमति सेटअप प्रणाली',
+        'multiple_email_and_sms_gateway_added' => 'कई ईमेल और एसएमएस गेटवे जोड़े गए',
+        'human_resource_system' => 'मानव संसाधन प्रणाली',
+        'complete_Bed_management_system' => 'पूर्ण बिस्तर प्रबंधन प्रणाली',
+        'medication_and_visits_system' => 'दवा और दौरे प्रणाली',
+        'case_manager_management_system' => 'केस प्रबंधक प्रबंधन प्रणाली',
+        'patient_separate_login_and_appointment_system' => 'रोगी का अलग लॉगिन और अपॉइंटमेंट सिस्टम',
+        'hospital_enquiry_system' => 'अस्पताल पूछताछ प्रणाली',
+        'parmacy_management_system' => 'फार्मेसी प्रबंधन प्रणाली',
+        'appointment_management_system' => 'नियुक्ति प्रबंधन प्रणाली',
+        'investigation_reports' => 'जांच रिपोर्ट',
+    ],
+
+    //footer keys
+    'footer' => [
+        'follow_us' => 'हमारा अनुसरण करें',
+        'twitter' => 'ट्विटर',
+        'facebook' => 'फेसबुक',
+        'linkedin' => 'लिंक्डइन',
+        'github' => 'गिटहब',
+        'made_with_by_infyOm_technologies' => 'InfyOm Technologies द्वारा निर्मित',
+        'we_are_group' => 'हम एक क्रिएटिव नर्ड्स का समूह हैं जो वेब और मोबाइल के लिए शानदार चीजें बना रहे हैं। हम ओपन सोर्स तकनीकों में योगदान देना पसंद करते हैं। हम हमेशा कुछ ऐसा बनाने की कोशिश करते हैं जो डेवलपर्स का समय बचाए ताकि वे अपने दोस्तों और परिवार के साथ और अधिक समय बिता सकें।',
+        'copyright' => 'कॉपीराइट',
+        'all_rights_reserved' => 'सर्वाधिकार सुरक्षित',
+    ],
+
+];
